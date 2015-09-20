e39976d37d3d6ce3afee6d8b93725d624352b648
Jeremy Evans <code@jeremyevans.net>
2015-09-20 11:41:15 -0700

    Add layout and form for uploading document images

--- slab.rb
+++ slab.rb
@@ -1,9 +1,19 @@
 require 'roda'
+require 'tilt/erubis'
 
 class Slab < Roda
+  plugin :render, :escape=>true
+  plugin :symbol_views
+
   route do |r|
     r.root  do
-      'Hello World'
+      r.redirect '/upload'
+    end
+    
+    r.is "upload" do
+      r.get do
+        :upload
+      end
     end
   end
 end

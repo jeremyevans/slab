0ee369bcf7ed73e9316e836b92313b42bb1ea184
Jeremy Evans <code@jeremyevans.net>
2015-09-20 12:46:59 -0700

    Add ability to view uploaded document images

--- slab.rb
+++ slab.rb
@@ -5,12 +5,30 @@ require './models'
 class Slab < Roda
   plugin :render, :escape=>true
   plugin :symbol_views
+  plugin :symbol_matchers
 
   route do |r|
     r.root  do
       r.redirect '/upload'
     end
     
+    r.get "documents" do
+      @docs = Document.select(:id, :path).all
+      :documents
+    end
+
+    r.on "document/:d" do |doc_id|
+      @doc = Document.with_pk!(doc_id)
+
+      r.get true do
+        :document
+      end
+
+      r.get 'image' do
+        @doc.image
+      end
+    end
+
     r.is "upload" do
       r.get do
         :upload

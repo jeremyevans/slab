dc65b8ec9218dec97ac55a2ee0cbf5b56a8103ee
Jeremy Evans <code@jeremyevans.net>
2015-09-26 16:50:19 -0700

    Allow bin/slabup to work for users using token auth

--- slab.rb
+++ slab.rb
@@ -5,7 +5,7 @@ require './models'
 class Slab < Roda
   use Rack::Session::Cookie, :key => '_slab_session',
     :secret=>(ENV['SLAB_SECRET'] ||= SecureRandom.base64(20))
-  plugin :csrf
+  plugin :csrf, :skip => ['POST:/slabup/\d+_[^/]+']
   plugin :render, :escape=>true
   plugin :symbol_views
   plugin :symbol_matchers
@@ -20,6 +20,13 @@ class Slab < Roda
   route do |r|
     r.on 'auth' do
       r.rodauth
+    end
+
+    r.post 'slabup/:token' do |token|
+      account_id, token = token.split('_', 2)
+      unless Account.where(:id=>account_id.to_i, :token=>token).empty?
+        handle_upload(account_id.to_i).id.to_s
+      end
     end
 
     r.redirect '/auth/login' unless rodauth.logged_in?
@@ -62,11 +69,16 @@ class Slab < Roda
       end
 
       r.post do
-        image = r['image']
-        d = Document.create(:account_id=>rodauth.session_value, :image=>image[:tempfile].read, :path=>image[:filename])
-        DB.notify('ocr', :payload=>d.id)
+        handle_upload(rodauth.session_value)
         r.redirect
       end
     end
+  end
+
+  def handle_upload(account_id)
+    image = request['image']
+    d = Document.create(:account_id=>account_id, :image=>image[:tempfile].read, :path=>image[:filename])
+    DB.notify('ocr', :payload=>d.id)
+    d
   end
 end
